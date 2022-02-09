20292e46bb99d9c7449c7ed29d24652ed00a5e3c
Samin Marzban <bmst555m@gmail.com>
2022-02-09 18:26:29 +0330

    add data class Customer and Main Class & maxBy

--- src/main/kotlin/q2.kt
+++ src/main/kotlin/q2.kt
@@ -23,3 +23,29 @@ enum class Gender {
     Female,
     Male
 }
+data class Customer(val name: String, val age: Int, val gender: Gender)
+class Main {
+    fun toString(input: Employee): String {
+        return input.javaClass.name + ": " + input.toString()
+    }
+
+    private var totalPayment = 0.0
+    fun totalPayment(input: List<Employee>, hours: List<Double>) {
+        for (employee in input) {
+            this.totalPayment += employee.salary(hours[input.indexOf(employee)])
+        }
+        println("The total payment of all is " + this.totalPayment)
+    }
+
+    val maxBy = { managers: MutableList<Manager> ->
+        var max = 0
+        var indexOfManager = 0
+        for (manager in managers) {
+            if (max < manager.age) {
+                max = manager.age
+                indexOfManager = managers.indexOf(manager)
+            }
+        }
+        println(managers[indexOfManager].toString())
+    }
+}
