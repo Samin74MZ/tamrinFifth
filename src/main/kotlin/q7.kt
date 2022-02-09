53b3b9c1e4e32601d769c497e8119da8920d681f
Samin Marzban <bmst555m@gmail.com>
2022-02-09 18:47:22 +0330

    start question 7 and write fun BinaryToDecimal

---
+++ src/main/kotlin/q7.kt
@@ -0,0 +1,31 @@
+import java.util.*
+import kotlin.math.pow
+
+object ChangeType{
+
+    fun changeToDecimal(input:Long):Int{
+        var number=input
+        var decimalNumber=0
+        var i=0
+        var remainder:Long
+        while (number.toInt()!=0){
+            remainder=number%10
+            number/=10
+            decimalNumber+=(remainder* 2.0.pow(i.toDouble())).toInt()
+            i++
+        }
+        return decimalNumber
+    }
+}
+fun main(){
+    val reader= Scanner(System.`in`)
+    println("What kind of number do you want to change?(B/D)")
+    val type= readLine()
+    println("Enter your number:")
+    if (type=="B"){
+        val number=reader.nextLong()
+        println("After change:")
+        println(ChangeType.changeToDecimal(number))
+    }
+
+}
