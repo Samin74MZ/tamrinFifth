498772d1969fde28becd23b858f7e770f7629158
Samin Marzban <bmst555m@gmail.com>
2022-02-09 21:00:00 +0330

    made two class implement for each interface

--- src/main/kotlin/q1.kt
+++ src/main/kotlin/q1.kt
@@ -10,4 +10,58 @@ interface PrivateTrasportationVehicle {
     fun maxSpeed()
     fun maxCapacity()
 }
+class Taxi:PublicTrasportationVehicle{
+    override fun maxSpeed() {
+
+    }
+
+    override fun maxCapacity() {
+
+    }
+}
+class Bus:PublicTrasportationVehicle{
+    override fun maxSpeed() {
+
+    }
+
+    override fun maxCapacity() {
+
+    }
+}
+class Ambulance:ServiceTrasportationVehicle{
+    override fun maxSpeed() {
+
+    }
+
+    override fun maxCapacity() {
+
+    }
+}
+class Truck:ServiceTrasportationVehicle{
+    override fun maxSpeed() {
+
+    }
+
+    override fun maxCapacity() {
+
+    }
+}
+class SportCar:PrivateTrasportationVehicle{
+    override fun maxSpeed() {
+
+    }
+
+    override fun maxCapacity() {
+
+    }
+}
+class  HatchBackCar:PrivateTrasportationVehicle{
+    override fun maxSpeed() {
+
+    }
+
+    override fun maxCapacity() {
+
+    }
+}
 fun main(){}
