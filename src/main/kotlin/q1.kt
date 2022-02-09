177b2f26f55a86c5a3d9c0132ab06553418394f6
Samin Marzban <bmst555m@gmail.com>
2022-02-09 20:55:31 +0330

    start question 1 make interfaces

---
+++ src/main/kotlin/q1.kt
@@ -0,0 +1,13 @@
+interface PublicTrasportationVehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+}
+interface ServiceTrasportationVehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+}
+interface PrivateTrasportationVehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+}
+fun main(){}
