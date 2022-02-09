25c0b6e047cf2ed258a94bb25371e87f6a4d49be
Samin Marzban <bmst555m@gmail.com>
2022-02-10 02:08:46 +0330

    final test

--- src/main/kotlin/q1.kt
+++ src/main/kotlin/q1.kt
@@ -26,61 +26,61 @@ interface PrivateTrasportationVehicle {
 }
 
 class Taxi : PublicTrasportationVehicle, Vehicle() {
-    public override fun maxSpeed(): Int {
+     override fun maxSpeed(): Int {
         return 100
     }
 
-    public override fun maxCapacity(): Int {
+     override fun maxCapacity(): Int {
         return 4
     }
 }
 
 class Bus : Vehicle(), PublicTrasportationVehicle {
-    public override fun maxSpeed(): Int {
+     override fun maxSpeed(): Int {
         return 100
     }
 
-    public override fun maxCapacity(): Int {
+     override fun maxCapacity(): Int {
         return 18
     }
 }
 
 class Ambulance : ServiceTrasportationVehicle, Vehicle() {
-    public override fun maxSpeed(): Int {
+     override fun maxSpeed(): Int {
         return 155
     }
 
-    public override fun maxCapacity(): Int {
+     override fun maxCapacity(): Int {
         return 5
     }
 }
 
 class Truck : Vehicle(), ServiceTrasportationVehicle {
-    public override fun maxSpeed(): Int {
+     override fun maxSpeed(): Int {
         return 90
     }
 
-    public override fun maxCapacity(): Int {
+     override fun maxCapacity(): Int {
         return 3
     }
 }
 
 class SportCar : PrivateTrasportationVehicle, Vehicle() {
-    public override fun maxSpeed(): Int {
+     override fun maxSpeed(): Int {
         return 200
     }
 
-    public override fun maxCapacity(): Int {
+     override fun maxCapacity(): Int {
         return 2
     }
 }
 
 class HatchBackCar : PrivateTrasportationVehicle, Vehicle() {
-    public override fun maxSpeed(): Int {
+     override fun maxSpeed(): Int {
         return 120
     }
 
-    public override fun maxCapacity(): Int {
+     override fun maxCapacity(): Int {
         return 4
     }
 }
@@ -88,17 +88,17 @@ class HatchBackCar : PrivateTrasportationVehicle, Vehicle() {
 fun main() {
 
     val carList = ArrayList<Vehicle>()
-    val listOfClass = listOf<Vehicle>(Taxi(), Truck(), Bus(), Ambulance(), SportCar(), HatchBackCar())
+    val listOfClass = listOf(Taxi(), Truck(), Bus(), Ambulance(), SportCar(), HatchBackCar())
     for (i in 0 until 10) {
         //creating randomly 10 different object from the Falafel,Pizza and Hamburger classes
         val randomIndex = Random.nextInt(listOfClass.size)
         carList.add(listOfClass[randomIndex])
     }
     println(carList)
-    var sortBySpeed = carList.sortByDescending { it -> it.maxSpeed() }
+    val sortBySpeed = carList.sortByDescending { it.maxSpeed() }
     println("After sort By Speed")
     println(sortBySpeed)
-    var sortByCapacity=carList.sortByDescending { it->it.maxCapacity() }
+    val sortByCapacity=carList.sortByDescending { it.maxCapacity() }
     println("After sort By capacity")
     println(sortByCapacity)
 }
