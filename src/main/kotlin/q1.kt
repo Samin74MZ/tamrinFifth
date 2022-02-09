7efcb4806d6566ac9927efb00f76e3a93497c3b7
Samin Marzban <bmst555m@gmail.com>
2022-02-10 02:04:14 +0330

    Build sorted list by max Speed

--- src/main/kotlin/q1.kt
+++ src/main/kotlin/q1.kt
@@ -1,67 +1,106 @@
+import kotlin.random.Random
+
+open class Vehicle : PrivateTrasportationVehicle, ServiceTrasportationVehicle, PublicTrasportationVehicle {
+    override fun maxSpeed(): Int {
+        return 111
+    }
+
+    override fun maxCapacity(): Int {
+        return 4
+    }
+}
+
 interface PublicTrasportationVehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed(): Int
+    fun maxCapacity(): Int
 }
+
 interface ServiceTrasportationVehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed(): Int
+    fun maxCapacity(): Int
 }
+
 interface PrivateTrasportationVehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed(): Int
+    fun maxCapacity(): Int
 }
-class Taxi:PublicTrasportationVehicle{
-    override fun maxSpeed() {
 
+class Taxi : PublicTrasportationVehicle, Vehicle() {
+    public override fun maxSpeed(): Int {
+        return 100
     }
 
-    override fun maxCapacity() {
-
-    }
-}
-class Bus:PublicTrasportationVehicle{
-    override fun maxSpeed() {
-
-    }
-
-    override fun maxCapacity() {
-
+    public override fun maxCapacity(): Int {
+        return 4
     }
 }
-class Ambulance:ServiceTrasportationVehicle{
-    override fun maxSpeed() {
 
+class Bus : Vehicle(), PublicTrasportationVehicle {
+    public override fun maxSpeed(): Int {
+        return 100
     }
 
-    override fun maxCapacity() {
-
-    }
-}
-class Truck:ServiceTrasportationVehicle{
-    override fun maxSpeed() {
-
-    }
-
-    override fun maxCapacity() {
-
+    public override fun maxCapacity(): Int {
+        return 18
     }
 }
-class SportCar:PrivateTrasportationVehicle{
-    override fun maxSpeed() {
 
+class Ambulance : ServiceTrasportationVehicle, Vehicle() {
+    public override fun maxSpeed(): Int {
+        return 155
     }
 
-    override fun maxCapacity() {
-
-    }
-}
-class  HatchBackCar:PrivateTrasportationVehicle{
-    override fun maxSpeed() {
-
-    }
-
-    override fun maxCapacity() {
-
+    public override fun maxCapacity(): Int {
+        return 5
     }
 }
-fun main(){}
+
+class Truck : Vehicle(), ServiceTrasportationVehicle {
+    public override fun maxSpeed(): Int {
+        return 90
+    }
+
+    public override fun maxCapacity(): Int {
+        return 3
+    }
+}
+
+class SportCar : PrivateTrasportationVehicle, Vehicle() {
+    public override fun maxSpeed(): Int {
+        return 200
+    }
+
+    public override fun maxCapacity(): Int {
+        return 2
+    }
+}
+
+class HatchBackCar : PrivateTrasportationVehicle, Vehicle() {
+    public override fun maxSpeed(): Int {
+        return 120
+    }
+
+    public override fun maxCapacity(): Int {
+        return 4
+    }
+}
+
+fun main() {
+
+    val carList = ArrayList<Vehicle>()
+    val listOfClass = listOf<Vehicle>(Taxi(), Truck(), Bus(), Ambulance(), SportCar(), HatchBackCar())
+    for (i in 0 until 10) {
+        //creating randomly 10 different object from the Falafel,Pizza and Hamburger classes
+        val randomIndex = Random.nextInt(listOfClass.size)
+        carList.add(listOfClass[randomIndex])
+    }
+    println(carList)
+    var sortBySpeed = carList.sortByDescending { it -> it.maxSpeed() }
+    println("After sort By Speed")
+    println(sortBySpeed)
+
+}
+
+
+
+
